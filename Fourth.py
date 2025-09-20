40d83dffdbcc7210f92ba46ad633257de338e99a
Jousei69 <jose22wei@gmail.com>
2025-09-20 13:37:55 +0700

    first commit

---
+++ Fourth.py
@@ -0,0 +1,5 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+print("Is the first number greater than the second?", num1 > num2)
+print("Are the two numbers equal?", num1 == num2)
---
+++ Second.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+print("Multiplication:", num1 * num2)
+if num2 != 0:
+    print("Division:", num1 / num2)
+else:
+    print("Division: Cannot divide by zero")
